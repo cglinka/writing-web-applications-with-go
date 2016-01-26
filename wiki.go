4a75f2930f7eaa81928a16c0cfdf57ef8bdf4a5f
Clare Glinka <clare.glinka@returnpath.com>
2016-01-26 16:00:41 -0600

    Add loadPage function

--- wiki.go
+++ wiki.go
@@ -18,6 +18,12 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
+
 func main() {
 
 }
